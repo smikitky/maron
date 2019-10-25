004542431dd52bcf255fbbf23f2c368de3cb4d74
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-10-25 16:59:37 +0900

    Improve test

--- src/formatReference.test.js
+++ src/formatReference.test.js
@@ -1,11 +1,19 @@
 const esmRequire = require('esm')(module);
-const { formatPages } = esmRequire('./formatReference');
+const { authorList, formatPages } = esmRequire('./formatReference');
 
-test('formatPages', () => {
-  expect(formatPages([1, 10])).toBe('1-10');
-  expect(formatPages([1, 10], { compact: true })).toBe('1-10');
-  expect(formatPages([15, 25])).toBe('15-25');
-  expect(formatPages([15, 25], { compact: true })).toBe('15-25');
-  expect(formatPages([100, 110])).toBe('100-110');
-  expect(formatPages([100, 110], { compact: true })).toBe('100-10');
+describe('authorList', () => {
+  test('basic', () => {
+    expect(authorList('J Doe, A Smith')).toBe('J Doe, A Smith');
+    expect(authorList('J Doe, A Smith', { max: 1 })).toBe('J Doe et al');
+  });
+});
+
+describe('formatPages', () => {
+  test('basic', () => {
+    expect(formatPages([1, 10])).toBe('1-10');
+    expect(formatPages([1, 10], { compact: true })).toBe('1-10');
+    expect(formatPages([15, 25], { compact: true })).toBe('15-25');
+    expect(formatPages([100, 110], { compact: true })).toBe('100-10');
+    expect(formatPages([980, 1001], { compact: true })).toBe('980-1001');
+  });
 });
