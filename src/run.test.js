9a8bea5340ec3b115e069c64834f5d6d47ab2eee
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-11-01 07:15:37 +0000

    Improve tests

--- src/run.test.js
+++ src/run.test.js
@@ -7,17 +7,16 @@ test('Run using init-template', async () => {
   const srcDir = path.resolve(__dirname, '..', 'init-template');
   const outDir = path.resolve(__dirname, '..', 'test-out');
   try {
-    const mockReporter = {
-      log: jest.fn(),
-      info: jest.fn(),
-      warn: jest.fn(),
-      error: jest.fn(),
-      output: jest.fn(),
-      section: jest.fn()
-    };
+    const mockReporter = {};
+    ['log', 'info', 'warn', 'error', 'output', 'section'].forEach(
+      m => (mockReporter[m] = jest.fn())
+    );
     fs.emptyDir(outDir);
     await run(srcDir, outDir, {}, mockReporter);
     expect(mockReporter.error).not.toBeCalled();
+    expect(mockReporter.output).toBeCalledWith('index.md');
+    expect(mockReporter.output).toBeCalledWith('index.html');
+    expect(mockReporter.output).toBeCalledWith('style.css');
   } finally {
     fs.remove(outDir);
   }
