1e1aef9cd4de8e74d3b07fb1e984852d713b0ccc
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-11-01 03:11:01 +0000

    Implement --init

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
+import path from 'path';
 import chokidar from 'chokidar';
 import dashdash from 'dashdash';
 import _ from 'lodash';
+import fs from 'fs-extra';
 import run from './run';
+import url from 'url'; // Node >= 10.12 required
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 const main = async () => {
   const parser = dashdash.createParser({
     options: [
+      { names: ['init'], type: 'bool', help: 'Initialize a new article.' },
       { names: ['src'], type: 'string', help: 'Source dir', default: './src' },
       { names: ['out'], type: 'string', help: 'Output dir', default: './out' },
       { names: ['watch', 'w'], type: 'bool', help: 'Watch source files' },
@@ -33,7 +39,24 @@ const main = async () => {
 
   if (options.help) {
     help();
-    process.exit(0);
+    return;
+  }
+
+  if (options.init) {
+    const { src } = options;
+    console.log(`Setting up a new article under ${src}...`);
+    try {
+      await fs.ensureDir(src);
+      await fs.copy(path.resolve(__dirname, '..', 'init-template'), src, {
+        overwrite: false,
+        errorOnExist: true
+      });
+      console.log(`Created an empty project under ${src}.`);
+    } catch (err) {
+      console.error('Error while copying templates.');
+      console.error('Is your src directory empty and writable?');
+    }
+    return;
   }
 
   const start = () => run(options.src, options.out, options);
